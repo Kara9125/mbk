1f38c59fe20f7ffd08f5da9a86d0712bbd6994b9
Michelle Maltbia <maltbia.michelle@gmail.com>
2016-02-27 11:32:52 -0800

    Adds inspiration types.

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,10 @@
 $(document).ready(function(){
-    $(document).keyup(function(e){
-        console.log(e);
-        if(e.keyCode === 37){
-            $('#player').animate({"left": "-=20px"}, "fast")   
-        } 
-        if (e.keyCode === 39){
-            $('#player').animate({"left": "+=20px"}, "fast")   
-        }
-    })
-
-    window.setInterval(function(){
-        $('.cloud').animate({"right": "+=20px"}, 'fast')
-    }, 20)
+    console.log('hello');
     
-    $('#play-btn').on('click', function(){
-      $('#start-modal').css('visibility', 'hidden');  
-    $('#background').prepend('<object data="images/comet.svg" type="image/svg+xml" height="150px" style="z-index:99;margin-top:-200px;"></object>')
+    $('.emotion').on('click', function(e){
+        e.preventDefault();
+        console.log('clicked');
+        $('#inspo-type').css('visibility', 'visible');
     })
+    
 }); //Closing Document Ready tags
