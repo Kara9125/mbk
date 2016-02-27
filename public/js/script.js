8aa0fa74f7881f780ecb8cfbebd15244c5aae0d2
Michelle Maltbia <maltbia.michelle@gmail.com>
2016-02-27 14:37:14 -0800

    Adds inspiration content.

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,36 @@
 $(document).ready(function(){
     console.log('hello');
     
-    $('.emotion').on('click', function(e){
+    happyVid = ['m5yCOSHeYn4', 'keibTBgigQ8', 'kZlXWp6vFdE'];
+    happySongs = ['']
+    
+        <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/58831841&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+    $('div.emotion').on('click', function(e){
         e.preventDefault();
-        console.log('clicked');
-        $('#inspo-type').css('visibility', 'visible');
+        var emotion = $(this)[0].id;
+        $('#inspo-content').css('visibility', 'visible');
+        $('#video-pill').on('click', function(e){
+            e.preventDefault;
+            $('#inspo-content').empty();
+            if (emotion === 'happy'){
+                for(i=0; i < happyVid.length; i++){
+                    console.log(happyVid[i]);
+                    if(i === 0){
+                        $('#video-content').append('<div class="col-xs-12"><iframe width="100%" height="315" src="https://www.youtube.com/embed/'+ happyVid[i]+'" frameborder="0" allowfullscreen></iframe></div>');
+                    }
+                    else{
+                        $('#video-content').append('<div class="col-xs-12 col-sm-4"><iframe width="100%" height="315" src="https://www.youtube.com/embed/'+ happyVid[i]+'" frameborder="0" allowfullscreen></iframe></div>')
+                    }
+                }
+            }
+        })
+        console.log(emotion);
+        
+
     })
+    
+    
+    
+    
     
 }); //Closing Document Ready tags
